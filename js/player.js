f6ffcbcdd8ff1c1b46bb1298b1ea636fa166033b
legendm-web <legendm@petchs.org>
2026-02-05 15:27:37 -0500

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,18 +1,16 @@
 const params = new URLSearchParams(location.search);
 const id = params.get("id");
 const platform = params.get("p");
-
 const frame = document.getElementById("player");
 
 if (platform === "yt") {
-  const pref = getPlayer();
-
-  if (pref === "youtube") {
-    frame.src = `https://www.youtube-nocookie.com/embed/${id}`;
-  } else {
-    frame.src = `https://yewtu.be/embed/${id}`;
-  }
+    const pref = typeof getPlayer === 'function' ? getPlayer() : "invidious";
+    if (pref === "youtube") {
+        frame.src = `https://www.youtube-nocookie.com/embed/${id}?autoplay=1`;
+    } else {
+        // Try a different embed instance if yewtu.be is down
+        frame.src = `https://inv.nadeko.net/embed/${id}`;
+    }
 } else {
-  // LBRY
-  frame.src = `https://odysee.com/$/embed/${id}`;
+    frame.src = `https://odysee.com/$/embed/${id}`;
 }
